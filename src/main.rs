baf3c12dea1bd564535a57932d01d2b2f8cb69de
Louis Lambeau <llambeau@quadrabee.com>
2022-08-03 00:09:14 +0200

    Some renaming.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use finitio::fio;
+use std::path::{Path, PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     cmd_parse()
@@ -6,11 +7,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn cmd_parse() -> Result<(), Box<dyn std::error::Error>> {
     // Parse FIO file
-    let mut idocs: Vec<fio::Schema> = Vec::new();
-    let idoc = fio::parse_schema("@import finitio/data").map_err(|e| format!("{}", e))?;
-    idocs.push(idoc);
+    let mut fios: Vec<fio::Schema> = Vec::new();
+    let fio = fio::parse_schema("@import finitio/data").map_err(|e| format!("{}", e))?;
+    fios.push(fio);
 
-    println!("{:?}", idocs);
+    println!("{:?}", fios);
 
     Ok(())
 }
