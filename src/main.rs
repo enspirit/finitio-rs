adc2633513eb10b92264a70b9e985a67001bfc78
Louis Lambeau <llambeau@enspirit.be>
2022-10-21 19:30:49 +0200

    Support for local imports.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use finitio::fio;
 use finitio::schema;
-use finitio::js;
+use std::collections::HashSet;
+use std::path::{Path, PathBuf};
 
 use std::fs;
 use clap::{Parser, Subcommand};
@@ -32,8 +33,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .expect("Should have been able to read the file");
 
             let mut fios: Vec<fio::Schema> = Vec::new();
+
+            // Parse entry point
             let main_fio = fio::parse_schema(&contents[..]).expect("Syntax error");
             fios.push(main_fio);
+
+            // Parse imports
+            if !fios[0].imports.is_empty() {
+                let base_dir = Path::new(filename).parent()
+                    .ok_or("base_dir could not be determined from source")?;
+                let mut included_files: HashSet<PathBuf> = HashSet::new();
+                let mut includes = fios[0]
+                    .imports
+                    .iter()
+                    .map(|p| (base_dir.join(&p.filename)))
+                    .collect::<Vec<_>>();
+                while !includes.is_empty() {
+                    let include = includes.remove(0);
+                    if included_files.contains(&include) {
+                        continue;
+                    }
+                    included_files.insert(include.clone());
+                    let contents = fs::read_to_string(include.clone())?;
+                    let fio = fio::parse_schema(&contents[..]).expect("Syntax error");
+                    let dir = include.parent().unwrap();
+                    includes.extend(fio.imports.iter().map(|inc| dir.join(&inc.filename)));
+                    fios.push(fio);
+                }
+            }
 
             let res = schema::Schema::from_fio(fios.iter());
             match res {
